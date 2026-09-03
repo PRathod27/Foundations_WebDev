7610cca21bfcdd5a5f2ef975d62522d1c7687f33
Prem Rathod <prem.rathod.2715@gmail.com>
2026-09-03 23:58:18 +0530

    js code with structure of tic tac toe is added

--- 3. Tic_Tac_Toe/script.js
+++ 3. Tic_Tac_Toe/script.js
@@ -1,4 +1,15 @@
 let boxes = document.querySelectorAll(".boxes");
+let turnX = true;
 boxes.forEach((box) => {
-  console.log(box);
+  box.addEventListener("click", () => {
+    if (turnX) {
+      box.innerText = "X";
+      box.style.color = "white";
+      turnX = false;
+    } else {
+      box.innerText = "O";
+      box.style.color = "red";
+      turnX = true;
+    }
+  });
 });
