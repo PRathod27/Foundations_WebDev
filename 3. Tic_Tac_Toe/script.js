8d4122369bb3e25e198352c998a0b7e079a18e97
Prem Rathod <prem.rathod.2715@gmail.com>
2026-09-06 00:27:44 +0530

    Updated html, css, js file for tic_tac_toe

--- 3. Tic_Tac_Toe/script.js
+++ 3. Tic_Tac_Toe/script.js
@@ -1,5 +1,17 @@
 let boxes = document.querySelectorAll(".boxes");
 let turnX = true;
+
+let WinnerCondition = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+
 boxes.forEach((box) => {
   box.addEventListener("click", () => {
     if (turnX) {
@@ -11,5 +23,25 @@ boxes.forEach((box) => {
       box.style.color = "red";
       turnX = true;
     }
+    checkWinner();
   });
 });
+
+function checkWinner() {
+  for (let condition of WinnerCondition) {
+    let box1 = boxes[condition[0]].innerText;
+    let box2 = boxes[condition[1]].innerText;
+    let box3 = boxes[condition[2]].innerText;
+
+    if (box1 !== "" && box2 !== "" && box3 !== "") {
+      if (box1 === box2 && box2 === box3) {
+        console.log("Winner" + box1);
+        showResult(box1);
+      }
+    }
+
+    console.log(condition[0]);
+  }
+}
+
+function showResult(winner) {}
