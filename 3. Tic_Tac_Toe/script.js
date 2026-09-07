6c0b057628e480efabbb3b78b92fbbeafde87b63
Prem Rathod <prem.rathod.2715@gmail.com>
2026-09-07 18:58:20 +0530

    Tic_Tac_Toe Project Completed

--- 3. Tic_Tac_Toe/script.js
+++ 3. Tic_Tac_Toe/script.js
@@ -3,6 +3,8 @@ let turn1 = document.querySelector(".turn1");
 let turn2 = document.querySelector(".turn2");
 let msg = document.querySelector(".msg");
 let span = document.querySelector("#result");
+let reset = document.getElementById("reset");
+let ng = document.getElementById("ng");
 let turnX = true;
 
 let WinnerCondition = [
@@ -15,6 +17,26 @@ let WinnerCondition = [
   [0, 4, 8],
   [2, 4, 6],
 ];
+
+reset.addEventListener("click", () => {
+  boxes.forEach((box) => {
+    box.innerText = "";
+    box.disabled = false;
+    box.classList.add("hover");
+    // box.classList.remove("b-s");
+    msg.classList.add("hide");
+  });
+});
+
+ng.addEventListener("click", () => {
+  boxes.forEach((box) => {
+    box.innerText = "";
+    box.disabled = false;
+    box.classList.add("hover");
+    // box.classList.remove("b-s");
+    msg.classList.add("hide");
+  });
+});
 
 boxes.forEach((box) => {
   box.addEventListener("click", () => {
@@ -45,6 +67,12 @@ function checkWinner() {
       if (box1 === box2 && box2 === box3) {
         console.log("Winner" + box1);
         showResult(box1);
+        boxes.forEach((box) => {
+          box.classList.add("b-s");
+        });
+        boxes[condition[0]].classList.remove("b-s");
+        boxes[condition[1]].classList.remove("b-s");
+        boxes[condition[2]].classList.remove("b-s");
       }
     }
 
@@ -53,5 +81,16 @@ function checkWinner() {
 }
 
 function showResult(winner) {
+  boxes.forEach((box) => {
+    box.disabled = true;
+    box.classList.remove("hover");
+  });
+
   msg.classList.remove("hide");
+  span.innerText = winner;
+  if (winner === "X") {
+    span.style.color = "black";
+  } else {
+    span.style.color = "red";
+  }
 }
